f8a4940bf41dcf100639423d0b92b251a106867b
Jun Aruga <jaruga@redhat.com>
2021-02-25 20:53:36 +0100

    Rename the before/after hooks's symbol on RSpec 2 to the one on RSpec 3.
    Rename :all to :context .
    Rename :each to :example .
    
    There is no used around hook.
    
    https://relishapp.com/rspec/rspec-core/v/2-99/docs/hooks/before-and-after-hooks
    https://relishapp.com/rspec/rspec-core/v/3-10/docs/hooks/before-and-after-hooks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -75,7 +75,7 @@ MESSAGE
     end
   end
 
-  config.before(:all) do
+  config.before(:context) do
     new_client do |client|
       client.query %[
         CREATE TABLE IF NOT EXISTS mysql2_test (
@@ -137,11 +137,11 @@ MESSAGE
     end
   end
 
-  config.before(:each) do
+  config.before(:example) do
     @client = new_client
   end
 
-  config.after(:each) do
+  config.after(:example) do
     @clients.each(&:close)
   end
 end
