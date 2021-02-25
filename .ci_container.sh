af59d697788355aa12009eedd4f8e1eb462544d3
Jun Aruga <jaruga@redhat.com>
2021-02-25 08:10:26 +0100

    Rename .travis_* files to .ci_*.
    We want to keep the file name as a CI commonly used file rather than depending
    on a CI service name.

--- .ci_container.sh
+++ .ci_container.sh
@@ -6,6 +6,6 @@ ruby -v
 bundle install --path vendor/bundle --without benchmarks development
 
 # Start mysqld service.
-bash .travis_setup_container.sh
+bash .ci_setup_container.sh
 
 bundle exec rake
